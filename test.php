b86f0d0dae8b3c514245b4716d4c80eccf8798a0
Matt Pascoe <mattpascoe135@gmail.com>
2017-08-02 20:30:27 +1000

    adding commit

---
+++ test.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "root"
+$password = "g4vu9x5u@123"
+
+$conn = new mysqli($servername, $username, $password);
+
+*/
+if ($conn->connect_error) {
+	die("Connection failed: ".$conn->connect_error);
+}
+echo "Connected successfully";
+*/
+?>
